c4312213456674b7e1c4c0d4e598808b9f022d9c
Ranjith Cheguri <theeverindian@gmail.com>
2018-10-30 19:23:02 -0700

    kafka using nodejs

---
+++ Backend/index.js
@@ -0,0 +1,44 @@
+//import the require dependencies
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var kafka = require('./kafka/client');
+//use cors to allow cross origin resource sharing
+app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
+app.use(bodyParser.json());
+
+//Allow Access Control
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+  });
+
+
+app.post('/book', function(req, res){
+
+    kafka.make_request('post_book',req.body, function(err,results){
+        console.log("index.js",'in result');
+        console.log(results);
+        if (err){
+            console.log("index.js","Inside err");
+            res.json({
+                status:"error",
+                msg:"System Error, Try Again."
+            })
+        }else{
+            console.log("index.js","Inside else");
+                res.json({
+                    updatedList:results
+                });
+                res.end();
+            }
+    });
+});
+//start your server on port 3001
+app.listen(3001);
+console.log("index.js","Server Listening on port 3001");
